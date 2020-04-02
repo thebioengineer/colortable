23f28478623d5ac0daf55d9f41172c316d2844be
Ellis Hughes <ehhughes@scharp.org>
2020-04-02 14:30:19 -0700

    fix tests

--- tests/testthat/test-set_styling.R
+++ tests/testthat/test-set_styling.R
@@ -1,7 +1,7 @@
 test_that("set_styling works on basic vectors", {
 
   c_vctr <- color_vctr(1:5, text_color = "green")
-  c_vctr_styled <- set_styling(1:5, text_color ="green")
+  c_vctr_styled <- set_styling(1:5, text_color = "green")
   c_vctr_conditioned <- set_styling(1:5, 1:5 > 4, text_color = "green")
 
   expect_equal(c_vctr, c_vctr_styled)
@@ -21,26 +21,29 @@ test_that("set_styling updates color_vctr styling", {
 })
 
 test_that("set_styling throw error when idx out of range", {
+  c_vctr <- color_vctr(1:5, text_color = "green")
 
-  expect_error(set_styling(1:5, rep(TRUE,6), text_color = "red"),
+  expect_error(set_styling(c_vctr, rep(TRUE,6), text_color = "red"),
                "Length of index must be same as input vector")
 
-  expect_error(set_styling(1:5, 12, text_color = "red"),
+  expect_error(set_styling(c_vctr, 12, text_color = "red"),
                "Indexes out of Range")
 
 })
 
 test_that("set_styling throw error when idx is not numeric/logical", {
+  c_vctr <- color_vctr(1:5, text_color = "green")
 
-  expect_error(set_styling(1:5, c("TRUE"), text_color = "red"),
+  expect_error(set_styling(c_vctr, c("TRUE"), text_color = "red"),
                "Invalid Index. Must be of type Numeric or Logical")
 
 })
 
 
 test_that("set_styling throw warning when idx duplicated", {
+  c_vctr <- color_vctr(1:5, text_color = "green")
 
-  expect_warning(set_styling(1:5, c(1,1), text_color = "red"),
+  expect_warning(set_styling(c_vctr, c(1,1), text_color = "red"),
                "Duplicated indexes provided")
 
 })
