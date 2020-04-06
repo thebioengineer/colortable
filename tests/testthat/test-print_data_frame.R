79dbbb8bb96bc822b6fc1fd01b30376a3e440164
thebioengineer <ellishughes@live.com>
2020-04-05 22:59:05 -0700

    add data.frame printing tests

--- tests/testthat/test-print_data_frame.R
+++ tests/testthat/test-print_data_frame.R
@@ -1,6 +1,6 @@
 
 capture_print <- function(x, method){
-  capture.output(print(x,console_width = 80, method = method))
+  capture.output(print(x, method = method))
 }
 
 sample_df <- data.frame(
@@ -16,6 +16,30 @@ test_that("console data.frame printing is formatted properly", {
       "2 2      \033[38;5;12mB\033[0m",
       "3 3      \033[38;5;12mC\033[0m",
       "4 4      \033[38;5;12mD\033[0m"
+    )
+  )
+})
+
+test_that("html data.frame printing is formatted properly", {
+  expect_equal(
+    capture_print(sample_df, method = "html"),
+    c("  x c_vctr",
+      "1 1      <span style='color:blue;'>A</span>",
+      "2 2      <span style='color:blue;'>B</span>",
+      "3 3      <span style='color:blue;'>C</span>",
+      "4 4      <span style='color:blue;'>D</span>"
+    )
+  )
+})
+
+test_that("latex data.frame printing is formatted properly", {
+  expect_equal(
+    capture_print(sample_df, method = "latex"),
+    c("  x c_vctr",
+      "1 1      \\textcolor{blue}{A}",
+      "2 2      \\textcolor{blue}{B}",
+      "3 3      \\textcolor{blue}{C}",
+      "4 4      \\textcolor{blue}{D}"
     )
   )
 })
