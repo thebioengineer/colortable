1d94784180f0b5e86bab79f9b462b86757892121
Ellis Hughes <ehhughes@scharp.org>
2020-04-19 09:53:32 -0700

    method for priting html and tex outputs

---
+++ R/knit_print.R
@@ -0,0 +1,57 @@
+#' knit_print method for color_vctrs
+#' @export
+#' @keywords internal
+#' @importFrom knitr knit_print asis_output
+knit_print.color_vctr <- function(x, inline = FALSE, ...){
+  if (inline) {
+    format(x, method = print_method())
+  }else{
+    asis_output(knit_vctr_output(x, ...))
+  }
+}
+
+#' @importFrom knitr asis_output knit_print
+knit_vctr_output <- function(x, ..., method = print_method(), print_width = options()$width){
+  formatted_x <- format(x, method = method)
+  formatted_x <- format_console_vctr_print(x, formatted_x, console_width = print_width)
+  pre_wrap(formatted_x, method = method)
+}
+
+pre_wrap <- function(x, ..., method = print_method()){
+  pre_wrap_method <- switch(
+    method,
+    "latex" = pre_wrap.latex,
+    "beamer" = pre_wrap.latex,
+    "html" = pre_wrap.html,
+    "gfm" = pre_wrap.html,
+    c
+  )
+  pre_wrap_method(x, ...)
+}
+
+pre_wrap.latex <- function(x,...){
+  c("\\begin{Verbatim}[commandchars=\\\\\\{\\}]\n",
+    paste0("## ",x,"\n"),
+    "\\end{Verbatim}\n")
+}
+
+pre_wrap.html <- function(x,...){
+  c("<pre>","<code class = \"hljs\">",
+    paste("<span>##",x,"</span><br>"),
+    "</code>","</pre>")
+}
+
+#' knit_print method for data.frames
+#' @export
+#' @keywords internal
+#' @importFrom knitr kable knit_print asis_output
+knit_print.data.frame <- function(x, options, ...){
+  printing_method <- print_method()
+  if(isTRUE(getOption("rstudio.notebook.executing"))){
+    printing_method = "html"
+  }
+  df_color_vctr <- as.data.frame(lapply(as.list(x),format, method = printing_method))
+  rownames(df_color_vctr) <- rownames(x)
+  formatted_table <- paste(kable(df_color_vctr),collapse = "\n")
+  asis_output(formatted_table)
+}
