c0963d5964a9906343255ee36b3ea83d9f89c3e5
thebioengineer <ellishughes@live.com>
2020-12-04 20:56:03 -0800

    clean up docs by adding rdname

--- R/as_color_vctr.R
+++ R/as_color_vctr.R
@@ -1,6 +1,8 @@
 #' Coerce object to a color_vctr
 #'
-#' Coersion function for generation of a color_vctr from an existing vector
+#' Coercion function for generation of a color_vctr from an existing vector
+#'
+#' @rdname as_color_vctr
 #'
 #' @param x object to coerce
 #' @param text_color A vector of length 1 or same length as vect. Details the
@@ -18,12 +20,14 @@ as_color_vctr <- function(x, text_color = NA, background = NA, style = NA){
   UseMethod("as_color_vctr",x)
 }
 
+#' @rdname as_color_vctr
 #' @export
 as_color_vctr.default <- function(x, text_color = NA, background = NA, style = NA){
   stop(gettextf("cannot coerce class %s to a color_vctr",
                 sQuote(deparse(class(x))[1L])), domain = NA)
 }
 
+#' @rdname as_color_vctr
 #' @export
 as_color_vctr.numeric <- function(x,text_color = NA, background = NA, style = NA){
   new_color_vctr(x,
@@ -32,17 +36,20 @@ as_color_vctr.numeric <- function(x,text_color = NA, background = NA, style = NA
                  style = style)
 }
 
+#' @rdname as_color_vctr
 #' @export
 as_color_vctr.color_vctr<- function(x,text_color = NA, background = NA, style = NA){
   x
 }
 
+#' @rdname as_color_vctr
 #' @export
 as_color_vctr.character <- as_color_vctr.numeric
 
 #' @export
 as_color_vctr.integer <- as_color_vctr.numeric
 
+#' @rdname as_color_vctr
 #' @export
 as_color_vctr.logical <- as_color_vctr.numeric
 
